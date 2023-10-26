4635d50600fab7852da871919ad575857b432577
barrientosjesus <jabarri1@gmail.com>
2023-10-26 16:31:37 -0700

    Add loading dots to Lobby

--- src/components/Lobby/Lobby.jsx
+++ src/components/Lobby/Lobby.jsx
@@ -1,5 +1,28 @@
-export default function Lobby() {
+export default function Lobby({ quiz, lobby, handleSubmit }) {
     return (
-        <h1>Lobby</h1>
-    )
+        <>
+            {quiz ?
+                <>
+                    <div className="col-span-12 flex flex-col items-center m-4">
+                        <div className="text-3xl text-white font-bold">{quiz?.title || 'Error'}</div>
+                        <div className="text-xl text-white">{quiz?.category || 'Error'}</div>
+                        <small className="text-white"><em>{quiz?.questions.length || 'Error'} Questions</em></small>
+                    </div>
+                    <div className="col-span-12 flex">
+                        {lobby && lobby.map((l, index) => (
+                            <div className="flex flex-col justify-center mx-3" key={index}>
+                                <img src={`https://api.dicebear.com/7.x/bottts/svg?seed=${l.name}&radius=50&scale=100`} alt="avatar" key={index} />
+                                {l.name}
+                            </div>
+                        ))}
+                    </div>
+                    <button onClick={handleSubmit} className="btn btn-primary shadow-lg col-span-12 self-end mb-8">Play!</button>
+                </>
+                :
+                <div className="col-span-12 rounded-md m-3 bg-violet-500 flex flex-col items-center justify-center">
+                    <span className="loading loading-dots loading-lg text-white"></span>
+                </div>
+            }
+        </>
+    );
 }
--- src/pages/LobbyPage/LobbyPage.jsx
+++ src/pages/LobbyPage/LobbyPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getOne } from "../../utilities/quiz-api";
+import Lobby from "../../components/Lobby/Lobby";
 import * as socket from "../../utilities/socket";
 
 export default function LobbyPage({ user }) {
@@ -8,7 +9,6 @@ export default function LobbyPage({ user }) {
     const [quiz, setQuiz] = useState(null);
     const [lobby, setLobby] = useState(null);
     const quizID = useParams().quizID;
-    const location = useLocation();
 
     useEffect(() => {
         socket.registerSetGame(setLobby);
@@ -26,24 +26,15 @@ export default function LobbyPage({ user }) {
         };
     }, []);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (question) {
-            socket.sendMessage(quizID, user.name, question);
-            setQuestion('');
-        }
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        alert('play button pressed!')
     };
 
     return (
         <div className="h-full w-full flex flex-col items-center justify-center">
-            <h2>{quiz?.title || 'Error'}</h2>
-            <div className="flex items-center">
-                {lobby && lobby.map((l, index) => (
-                    <div className="flex flex-col justify-center mx-3" key={index}>
-                        <img src={`https://api.dicebear.com/7.x/bottts/svg?seed=${l.name}&radius=50&scale=100`} alt="avatar" />
-                        {l.name}
-                    </div>
-                ))}
+            <div className="grid grid-cols-12 rounded-md justify-items-center grid-rows-3 h-1/2 w-1/2 bg-violet-500 shadow-md">
+                <Lobby quiz={quiz} lobby={lobby} handleSubmit={handleSubmit} />
             </div>
         </div>
     );
