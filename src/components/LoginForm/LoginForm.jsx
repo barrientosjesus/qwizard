0118ff61b7cc641eac8c2bb82ea36fdd3676bddf
barrientosjesus <jabarri1@gmail.com>
2023-10-29 16:22:51 -0700

    Update auth page styling to be more responsive. Change avatar image placement dependant on screen size

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -38,19 +38,22 @@ export default function LoginForm({ setUser }) {
 
   return (
     <>
-      <div className="hidden lg:mt-0 lg:col-span-5 lg:flex max-h-[520px]">
-        <img src="https://i.imgur.com/NqXoaX6.png" alt="mockup" />
+      <div className="hidden lg:mt-0 md:col-span-5 md:flex max-h-[520px] justify-center items-center">
+        {showSignUp ?
+          <img src={`https://api.dicebear.com/7.x/bottts/svg?seed=${credentials.name}&radius=50&scale=64`} className='bg-violet-500/80 rounded-full' alt="avatar" />
+          :
+          <img src="https://i.imgur.com/NqXoaX6.png" alt="mockup" />
+        }
       </div>
-      <div className="lg:col-start-7 lg:col-span-6 z-10 flex flex-col items-center lg:place-self-center w-72 bg-violet-500 shadow-lg lg:h-full lg:w-3/5 lg:items-center lg:justify-center rounded-lg">
-        <form autoComplete="off" onSubmit={handleSubmit} name={showSignUp ? 'signup' : 'login'} className='flex flex-col items-center p-10'>
-          <div className='flex items-center justify-center w-full'>
-            {showSignUp && <img src={`https://api.dicebear.com/7.x/bottts/svg?seed=${credentials.name}&radius=50&scale=25`} alt="avatar" />}
-            <h2 className='font-bold text-white text-3xl m-3 text-center'>{showSignUp ? 'Sign Up' : 'Log In'}</h2>
-            {showSignUp && <img src={`https://api.dicebear.com/7.x/bottts/svg?seed=${credentials.name}&radius=50&scale=25`} alt="avatar" />}
+      <div className="col-span-12 md:col-start-7 md:col-span-5 z-10 flex flex-col lg:place-self-center bg-violet-500/90 shadow-lg lg:h-full w-full lg:items-center lg:justify-center rounded-lg">
+        <form autoComplete="off" onSubmit={handleSubmit} name={showSignUp ? 'signup' : 'login'} className='flex flex-col items-center p-2'>
+          <div className='flex items-center justify-center'>
+            <p className='font-bold text-white text-3xl text-center'>{showSignUp ? 'Sign Up' : 'Log In'}</p>
           </div>
+          <img src={`https://api.dicebear.com/7.x/bottts/svg?seed=${credentials.name}&radius=50`} className='flex h-[72px] md:hidden' alt="avatar" />
           {showSignUp &&
             <>
-              <label className='text-white'>Name</label>
+              <label className='text-white flex justify-center items-center mt-4'>Name</label>
               <input type="text" name="name" className='input input-bordered input-secondary w-full max-w-xs' value={credentials.name} onChange={handleChange} required />
             </>
           }
@@ -64,11 +67,11 @@ export default function LoginForm({ setUser }) {
               <input type="password" name="confirm" className='input input-bordered input-secondary w-full max-w-xs focus:border-none' value={credentials.confirm} onChange={handleChange} required />
             </>
           }
-          <button type="submit" className='btn btn-secondary m-4 w-full'>{showSignUp ? 'Sign Up' : 'Log In'}</button>
+          <button type="submit" className='btn btn-secondary m-4 w-1/2 md:w-2/6 lg:w-full'>{showSignUp ? 'Sign Up' : 'Log In'}</button>
           <p className="error-message">&nbsp;{error}</p>
+          <hr className='w-1/2 md:w-2/6 lg:w-full -mt-8 mb-5 text-white' />
+          <button className='btn btn-neutral mb-8 w-1/2 md:w-2/6 lg:w-full' onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
         </form>
-        <hr className='w-1/2 -mt-8 mb-5 text-white' />
-        <button className='btn btn-neutral mb-8' onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
       </div>
     </>
   );
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -3,8 +3,8 @@ import LoginForm from '../../components/LoginForm/LoginForm';
 export default function AuthPage({ setUser }) {
 
   return (
-    <main className='flex items-center'>
-      <div className="grid mr-auto max-w-screen-xl px-4 py-8 mx-auto items-center lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:z-10">
+    <main className='items-center grid grid-cols-12 justify-items-center w-full'>
+      <div className="grid grid-cols-12 max-w-screen-xl py-8 items-center lg:gap-8 xl:gap-0 lg:py-16 col-span-12 lg:z-10 w-full">
         <LoginForm setUser={setUser} />
       </div>
     </main>
