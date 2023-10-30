9111f07208cecf03a3f964d5ee64236eb263cbb9
barrientosjesus <jabarri1@gmail.com>
2023-10-30 13:53:13 -0700

    Add randomization to question mark background

--- src/components/QuestionMarkBG/QuestionMarkBG.jsx
+++ src/components/QuestionMarkBG/QuestionMarkBG.jsx
@@ -2,29 +2,37 @@ import "./QuestionMarkBG.css"
 
 export default function QuestionMarkBG() {
 
+    function randomStyles(){
+        return {
+            fontSize: (Math.floor(Math.random() * 7) + 5) + "rem",
+            animationDelay: Math.floor(Math.random() * 14) + "s",
+            animationDuration: (Math.floor(Math.random() * 15) + 30) + "s",
+        }
+    }
+
     return (
         <div className="area QuestionMarkBG" >
             <ul className="circles">
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
-                <li className="font-bold qm-bg text-9xl">?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
+                <li className="font-bold qm-bg text-9xl" style={randomStyles()}>?</li>
             </ul>
         </div >
     );
